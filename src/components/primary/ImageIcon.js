6eb147e124f8a1d84c835a71cdaf84cf1c500292
Maroof Shittu <dbugshe2@gmail>
2020-03-23 12:02:38 +0100

    Add Icons Assets and TabBar dependencies

--- src/components/primary/ImageIcon.js
+++ src/components/primary/ImageIcon.js
@@ -1,29 +1,33 @@
-// import React from "react";
-// import { Image, StyleSheet } from "react-native";
-// import icons from "../../constants/icons";
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import icons from "../../constants/icons";
 
-// const ImageIcon = props => {
-//   const { name, style, children } = props;
-//   const { icon, height, width, absolute, top, right, bottom, left } = icons[
-//     `${name}`
-//   ];
-//   const iconStyles = StyleSheet.flatten(
-//     {
-//       height: height,
-//       width: width
-//     },
-//     absolute && { position: absolute },
-//     top && {top},
-//     right && {right},
-//     bottom && { bottom},
-//     left && {left},
-//     style
-//   );
-//   return (
-//     <Image source={icon} style={iconStyles} resizeMode="contain" {...props}>
-//       {children}
-//     </Image>
-//   );
-// };
+const ImageIcon = props => {
+  const { name, style, children } = props;
+  const { icon, height, width, absolute, top, right, bottom, left } = icons[
+    `${name}`
+  ];
+  const iconStyles = StyleSheet.flatten(
+    {
+      height: height,
+      width: width
+    },
+    absolute && { position: absolute },
+    top && {top},
+    right && {right},
+    bottom && { bottom},
+    left && {left},
+    style
+  );
+  return (
+    <Image source={icon} style={iconStyles} resizeMode="contain" {...props}>
+      {children}
+    </Image>
+  );
+};
 
-// export default ImageIcon;
+ImageIcon.defaultProps = {
+  name: 'add',
+}
+
+export default ImageIcon;
---
+++ src/constants/icons.js
@@ -0,0 +1,69 @@
+const icons = {
+  add: {
+    icon: require("../assets/icons/add"),
+    width: 17,
+    height: 17
+  },
+  back: {
+    icon: require("../assets/icons/back"),
+    width: 16,
+    height: 15
+  },
+  copy: {
+    icon: require("../assets/icons/copy"),
+    width: 12,
+    height: 12
+  },
+  event: {
+    icon: require("../assets/icons/event"),
+    width: 23,
+    height: 23
+  },
+  eventAlt: {
+    icon: require("../assets/icons/event-alt"),
+    width: 23,
+    height: 23
+  },
+  house: {
+    icon: require("../assets/icons/house"),
+    width: 23,
+    height: 21
+  },
+  houseAlt: {
+    icon: require("../assets/icons/house-alt"),
+    width: 23,
+    height: 21
+  },
+  recieved: {
+    icon: require("../assets/icons/revieved"),
+    width: 10,
+    height: 11
+  },
+  sent: {
+    icon: require("../assets/icons/sent"),
+    width: 10,
+    height: 11
+  },
+  storeFront: {
+    icon: require("../assets/icons/store-front"),
+    width: 23,
+    height: 20
+  },
+  storeFrontAlt: {
+    icon: require("../assets/icons/store-front-alt"),
+    width: 23,
+    height: 20
+  },
+  trend: {
+    icon: require("../assets/icons/trend"),
+    width: 27,
+    height: 16
+  },
+  trendAlt: {
+    icon: require("../assets/icons/trend-alt"),
+    width: 27,
+    height: 16
+  }
+};
+
+export default icons;
