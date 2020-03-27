af9c60410a27c7162f6aec8ecc44353f442cdb7b
jonathan johnson <32993946+kxngnathan@users.noreply.github.com>
2020-03-27 18:50:40 +0100

    Icons and Market Screen

--- src/screens/Market/MarketPrice.js
+++ src/screens/Market/MarketPrice.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react'
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  ScrollView
+} from "react-native";
+import {Block, Card, Text, Header, ImageIcon, Input, Button, FAB} from '../../components'
+import { SIZES, COLORS } from '../../utils/theme'
+import BackButton from '../../components/BackButton';
+import { Divider } from 'react-native-paper';
 
-const MarketPrice = () => {
+
+const MarketPrice = ({navigation}) => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+   <Block   background center middle>
+   <Block   background center middle>
+      <ImageIcon  name="market"  />
+      <Text muted small>Update Daily Market Price & Earn credit points</Text>
+    </Block>  
+    <FAB right={15} bottom={35} onPress={() => navigation.navigate("PriceUpdate")}/>
+  </Block>
   )
 }
 
