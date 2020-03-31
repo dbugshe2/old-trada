cce59c5762ce6aaeb7770f17fda3c49cd890e863
jonathan johnson <32993946+kxngnathan@users.noreply.github.com>
2020-03-31 14:07:25 +0100

    leaderBoard screen

---
+++ src/data/index.js
@@ -0,0 +1,102 @@
+export const data= [
+    {
+      id: '1',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '2',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '3',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '4',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '5',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '6',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '7',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '8',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '9',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '10',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '1',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '2',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '3',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '4',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '5',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '6',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '7',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '8',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '9',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+    {
+      id: '10',
+      title: 'Charles barcly',
+      amount: '2,300',
+    },
+  ];
--- src/screens/Leaderboard/Leaderboard.js
+++ src/screens/Leaderboard/Leaderboard.js
@@ -1,12 +1,47 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {  View, FlatList, StyleSheet } from 'react-native'
+import { Block, Text, ImageIcon } from '../../components';
+import { SIZES } from '../../utils/theme'
+import { data } from '../../data/index';
 
 const Leaderboard = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  )
-}
 
-export default Leaderboard
+
+  return (
+    <Block  paddingHorizontal={SIZES.base}>
+      <Block flex>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) =>
+            `item-${index}`
+          }
+          renderItem={({item}) => {
+        return (
+          <Block row  middle center marginVertical={20} paddingHorizontal={SIZES.padding}>
+            
+            <Block>
+              <Text gray h6>
+                {item.title}
+              </Text>
+              </Block>
+              <ImageIcon  
+            style={{  
+                  
+                }}
+                name="coin" 
+            />
+            <Text gray paddingLeft={12} h6>
+              {item.amount}
+            </Text>
+            </Block>
+
+        );
+          }}
+        />
+      </Block>
+    </Block>
+  );
+ }
+
+
+export default Leaderboard;
